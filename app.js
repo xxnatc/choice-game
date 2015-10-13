b29b1d478cd97c019e52fd8ece94881108ae0149
rastringer <ra1stringer@gmail.com>
2015-10-12 21:39:17 -0700

    adds photos, style, builds JS functions & objects

--- app.js
+++ app.js
@@ -4,24 +4,31 @@
 var Photo = function(name, path) {
   this.name = name;
   this.path = path;
+  this.imagesArray = ["img/dale-cooper.jpg", "img/audrey-horne.jpg", "img/laura-palmer.jpg",
+   "img/log-lady.jpg", "img/donna-hayward.jpg", "img/harry-truman.jpg"]
+
+
+this.displayImage = function() {
+    var imagePrint = '<img src="' + path + Photo + '" alt = "">';
+    var num = Math.floor(Math.random() * (imagesArray.length + 1));
+    document.pics.src = imagesArray[num];
+    return displayImage;
+
+};
+
+
 }
 
-var dale = new Photo("Dale Cooper", "dale-cooper.jpg");
-var laura = new Photo("Laura Palmer", "laura-palmer.jpg");
-var donna = new Photo("Donna Hayward", "donna-hayward.jpg");
-var harry = new Photo("Harry S. Truman", "harry-truman.jpg");
-var logLady = new Photo("Lady with the log", "log-lady.jpg")
-var audrey = new Photo("Audrey Horne",  "audrey-horne.jpg")
+var dale = new Photo("Dale Cooper", "img/dale-cooper.jpg");
+var laura = new Photo("Laura Palmer", "img/laura-palmer.jpg");
+var donna = new Photo("Donna Hayward", "img/donna-hayward.jpg");
+var harry = new Photo("Harry S. Truman", "img/harry-truman.jpg");
+var lucy = new Photo("Lucy Moran", "img/Lucy Moran.jpg")
+var audrey = new Photo("Audrey Horne",  "img/audrey-horne.jpg")
+console.log(Photo(dale, laura, donna, harry, lucy, audrey));
 
 
 
-var imagesArray = ["dale-cooper.jpg", "audrey-horne.jpg", "laura-palmer.jpg",
-   "log-lady.jpg", "donna-hayward.jpg", "harry-truman.jpg"]
-
-function displayImage() {
-    var num = Math.floor(Math.random() * (imagesArray.length+1));
-    document.getElementById("canvas").src = imagesArray[num];
-}
 
 // function chooseImage(images_array, path) {
 //   var path = "img";
